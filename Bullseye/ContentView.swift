7107175fb1d97cbcfaba06d4884046a782cb8a6e
Parley Martins <parleypachecomartins@gmail.com>
2019-11-21 21:32:24 +0100

    Add some functions for round and score calculation

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,35 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State var slider: Double = 50.0
     @State var showResult: Bool = false
-    @State var target = Int.random(in: 1...100)
-    var score = 999
-    var round = 123
+    @State var target: Int = Int.random(in: 1...100)
+    @State var score = 0
+    @State var round = 1
     
     var body: some View {
         VStack {
             HStack {
-                Text("Put the bullseye as close as you can to:")
+                Text("Put the bullseye as close as you can to: ")
                 Text("\(self.target)")
             }.padding(.top, 20)
             Spacer()
             HStack {
                 Text("1")
-                Slider(value: self.$slider, in: 1...100)
+                Slider(value: $slider, in: 1...100)
                 Text("100")
             }
             Spacer()
-            Button(action:{ self.showResult = true }){
+            Button(action: hitme){
                 Text("Hit me")
             }.alert(isPresented: $showResult) { () -> Alert in
-                let rounded =  Int(self.slider.rounded())
-                return Alert(title: Text("Your score"), message: Text("\(rounded)"))
+                let bonus = getBonusPoints()
+                return Alert(
+                    title: Text(alertTitle()),
+                    message: Text("The slider value is \(roundSlider()). Your scored \(getRoundScore() + bonus) this round"),
+                    dismissButton: .default(Text("New round")){self.startRound(bonus: bonus)}
+                )
             }
             Spacer()
             HStack {
-                Button(action: self.restart) {
+                Button(action: startOver) {
                     Text("Start over")
                 }
                 Spacer()
@@ -58,9 +61,61 @@ struct ContentView: View {
         }
     }
     
-    func restart() -> Void {
-        self.target = Int.random(in: 1...100)
-        self.slider = Double.random(in: 1...100)
+    func startOver() -> Void {
+        startRound()
+        score = 0
+        round = 0
+    }
+    
+    func startRound(bonus: Int = 0) -> Void {
+        score += getRoundScore() + bonus
+        round += 1
+        target = Int.random(in: 1...100)
+        slider = Double.random(in: 1...100)
+    }
+    
+    func getRoundScore() -> Int {
+        return 100 - difference()
+    }
+    
+    func hitme() -> Void {
+        showResult = true
+    }
+    
+    func roundSlider() -> Int {
+        return Int(slider.rounded())
+    }
+    
+    func difference() -> Int {
+        return abs(roundSlider() - target)
+    }
+    
+    func alertTitle() -> String {
+        let difference = self.difference()
+        var title: String
+        if(difference == 0) {
+            title = "Perfect"
+        } else if (difference < 5) {
+            title = "Almost there!"
+        } else if (difference <= 10) {
+            title = "Not bad"
+        } else {
+            title = "Are you even trying?"
+        }
+        return title
+    }
+
+    func getBonusPoints() -> Int {
+        let difference = self.difference()
+        if(difference == 0) {
+            return 100
+        }
+        
+        if(difference == 1) {
+            return 50
+        }
+        
+        return 0
     }
     
 }
